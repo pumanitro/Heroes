6dbb6a077ec1e032fd911c021cc114da32be82d7
pumanitro <pumanitro@gmail.com>
2017-04-26 10:56:01 +0200

    Splitted app component into heroes component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from './Hero';
-import { HeroService } from './hero.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Tour of heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Spiderman'
-  };
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) {}
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-    this.selectedHero = this.heroes[0];
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-}
+export class AppComponent {}
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../Hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
+  title = 'Tour of heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Spiderman'
+  };
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => {
+      this.heroes = heroes;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
