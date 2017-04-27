45135985478ff8577bc3cca8a7dfec37720b096a
pumanitro <pumanitro@gmail.com>
2017-04-27 14:40:07 +0200

    Displaying details of given hero.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input} from '@angular/core';
 import {Hero} from '../Hero';
 
+// For routes:
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -10,9 +16,14 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService,
+              private route: ActivatedRoute,
+              private location: Location) { }
 
   ngOnInit() {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
   }
 
 }
